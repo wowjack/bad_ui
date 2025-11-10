63f9deaec91c4359746e4d43a4f8afdafa2d8e53
Jack Kingham <wowknead@gmail.com>
2025-11-09 21:25:50 -0700

    Eleventy config base path fix.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/css": "css"});
   eleventyConfig.addPassthroughCopy({"src/js": "js"});
   return {
+    pathPrefix: "/bad_ui/",
     dir: {
       input: "src",
       output: "dist",
